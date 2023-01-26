607e04fa6e16e32704bd1e0e50a241c685475a21
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-26 00:27:10 -0400

    Titulos en componentes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,9 @@
 const Formulario = () => {
     return ( 
-        <h1>Formulario</h1>
+        <div className="md:w-1/2 lg:w-3/5 text-center">
+            <h2 className="font-black text-xl">Seguimiento paciente</h2>
+            <p className="text-lg mt-4">Añade Pacientes y <span className="text-blue-500 font-bold">Administralos</span></p>
+        </div>
     );
 }
 
