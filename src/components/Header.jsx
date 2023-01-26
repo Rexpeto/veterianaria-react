f1b1ac3cba491344883e9d0718be22f99d65bcc2
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-26 00:27:36 -0400

    Titulos de la página

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 const Header = () => {
     return ( 
-        <h1>Header</h1>
+        <>
+            <h1 className="mt-3 text-3xl text-center font-bold">Seguimiento Pacientes <span className="text-blue-500">Veterinaria</span></h1>
+        </>
     );
 }
 
