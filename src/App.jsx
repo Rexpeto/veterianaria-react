ca3855b3ea3704c40c75c527f91b2d9591db721c
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-26 00:26:43 -0400

    Utilizando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import ListadoPaciente from "./components/ListadoPaciente";
 
-  return (
-    <div className="App">
-      <h1>Hola</h1>
-    </div>
-  )
+function App() {
+    return (
+        <div className="container mx-auto">
+            <Header/>
+            <div className="mt-12 md:flex">
+                <Formulario/>
+                <ListadoPaciente/>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
