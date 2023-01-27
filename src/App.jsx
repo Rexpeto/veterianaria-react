7d395c7d20d88f342246f3dffc5d22036b75f9e0
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-26 23:25:58 -0400

    useState de pacientes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPaciente from "./components/ListadoPaciente";
 
 function App() {
+    const [pacientes, setPacientes] = useState([]);
+
     return (
         <div className="container mx-auto">
             <Header/>
             <div className="mt-12 md:flex gap-4">
-                <Formulario/>
+                <Formulario setPacientes={setPacientes} pacientes={pacientes}/>
                 <ListadoPaciente/>
             </div>
         </div>
