91a7ae1d0e11beea066873f7d869222b3736d8c5
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-27 00:00:56 -0400

    Mostrando la información de los pacientes

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,15 +1,16 @@
-const Paciente = () => {
+const Paciente = ({paciente}) => {
+    const {nombre, propietario, email, alta, sintomas} = paciente;
     return ( 
         <div className="mt-5 bg-white rounded-md p-4">
-            <p className="text-md mx-2 mb-2 font-bold">Nombre: <span className="normal-case font-normal">Hook</span></p>
+            <p className="text-md mx-2 mb-2 font-bold">Nombre: <span className="normal-case font-normal">{nombre}</span></p>
 
-            <p className="text-md mx-2 mb-2 font-bold">Propietario: <span className="normal-case font-normal">Carlos Gallardo</span></p>
+            <p className="text-md mx-2 mb-2 font-bold">Propietario: <span className="normal-case font-normal">{propietario}</span></p>
 
-            <p className="text-md mx-2 mb-2 font-bold">Correo electronico: <span className="normal-case font-normal">Carlos@correo.com</span></p>
+            <p className="text-md mx-2 mb-2 font-bold">Correo electronico: <span className="normal-case font-normal">{email}</span></p>
 
-            <p className="text-md mx-2 mb-2 font-bold">Fecha de alta: <span className="normal-case font-normal">23/03/23</span></p>
+            <p className="text-md mx-2 mb-2 font-bold">Fecha de alta: <span className="normal-case font-normal">{alta}</span></p>
 
-            <p className="text-md mx-2 mb-2 font-bold">Sintomas: <span className="normal-case font-normal">Solo come y caga</span></p>
+            <p className="text-md mx-2 mb-2 font-bold">Sintomas: <span className="normal-case font-normal">{sintomas}</span></p>
         </div>
     );
 }
