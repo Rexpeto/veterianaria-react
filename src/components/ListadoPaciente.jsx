d232635ef1a06ae675ddd9e124c05a9fbed5d8b4
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-27 18:24:06 -0400

    Muestra un mensaje condicional

--- src/components/ListadoPaciente.jsx
+++ src/components/ListadoPaciente.jsx
@@ -4,13 +4,20 @@ const ListadoPaciente = ({pacientes}) => {
     return ( 
         <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
             <h2 className="font-black text-xl text-center">Listado de Pacientes</h2>
-            <p className="text-lg mt-4 text-center">Administra tus <span className="text-blue-500 font-bold">Pacientes y Citas</span></p>
+            
 
-            {pacientes.map(paciente => {
-                return (
-                    <Paciente key={paciente.id} paciente={paciente} />
-                )
-            })}
+            {pacientes && pacientes.length ? (
+                <>
+                    <p className="text-lg mt-4 text-center">Administra tus <span className="text-blue-500 font-bold">Pacientes y Citas</span></p>
+                    {pacientes.map(paciente => {
+                        return (
+                            <Paciente key={paciente.id} paciente={paciente} />
+                        )
+                    })}
+                </>
+            ) : (
+                <p className="text-lg mt-4 text-center">Comienza agregando pacientes y <span className="text-blue-500 font-bold">aparecenrán en este lugar</span></p>
+            )}
         </div>
     );
 }
