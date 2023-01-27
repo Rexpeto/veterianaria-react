984edf4261219a697b566c18d28008a7c8809890
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-27 18:40:13 -0400

    Botones editar y eliminar

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -11,6 +11,11 @@ const Paciente = ({paciente}) => {
             <p className="text-md mx-2 mb-2 font-bold">Fecha de alta: <span className="normal-case font-normal">{alta}</span></p>
 
             <p className="text-md mx-2 mb-2 font-bold">Sintomas: <span className="normal-case font-normal">{sintomas}</span></p>
+
+            <div className="flex gap-4 mt-5 items-center justify-between">
+                <button type="button" className="py-2 px-8 bg-yellow-500 rounded-lg shadow-md text-white font-bold hover:bg-yellow-600 transition-colors duration-200 uppercase text-sm">Editar</button>
+                <button type="button" className="py-2 px-10 bg-red-500 rounded-lg shadow-md text-white font-bold hover:bg-red-600 transition-colors duration-200 uppercase text-sm">Eliminar</button>
+            </div>
         </div>
     );
 }
