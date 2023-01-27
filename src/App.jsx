85336035cc29a0a1bfb43a98957944b569b76df3
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-26 23:59:33 -0400

    Pasandole pacientes a ListadoPaciente

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
             <Header/>
             <div className="mt-12 md:flex gap-4">
                 <Formulario setPacientes={setPacientes} pacientes={pacientes}/>
-                <ListadoPaciente/>
+                <ListadoPaciente pacientes={pacientes}/>
             </div>
         </div>
     );
