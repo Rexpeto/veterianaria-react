e58467ec8a20754ac834a8fbfbc02ce89f00ea23
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-28 18:07:02 -0400

    Manda paciente a formulario

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,5 +1,6 @@
-const Paciente = ({paciente}) => {
-    const {nombre, propietario, email, alta, sintomas} = paciente;
+const Paciente = ({pacientes, setPaciente}) => {
+    const {nombre, propietario, email, alta, sintomas} = pacientes;
+
     return ( 
         <div className="mt-5 bg-white rounded-md p-4">
             <p className="text-md mx-2 mb-2 font-bold">Nombre: <span className="normal-case font-normal">{nombre}</span></p>
@@ -13,7 +14,7 @@ const Paciente = ({paciente}) => {
             <p className="text-md mx-2 mb-2 font-bold">Sintomas: <span className="normal-case font-normal">{sintomas}</span></p>
 
             <div className="flex gap-4 mt-5 items-center justify-between">
-                <button type="button" className="py-2 px-8 bg-yellow-500 rounded-lg shadow-md text-white font-bold hover:bg-yellow-600 transition-colors duration-200 uppercase text-sm">Editar</button>
+                <button type="button" className="py-2 px-8 bg-yellow-500 rounded-lg shadow-md text-white font-bold hover:bg-yellow-600 transition-colors duration-200 uppercase text-sm" onClick={() => setPaciente(pacientes)}>Editar</button>
                 <button type="button" className="py-2 px-10 bg-red-500 rounded-lg shadow-md text-white font-bold hover:bg-red-600 transition-colors duration-200 uppercase text-sm">Eliminar</button>
             </div>
         </div>
