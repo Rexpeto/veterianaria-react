78f2b74440071cd6b6a674ed9c479f129eb40d21
Rexpeto <Carlosgallardoparra@hotmail.com>
2023-01-28 18:07:32 -0400

    state de paciente

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,21 @@ import ListadoPaciente from "./components/ListadoPaciente";
 
 function App() {
     const [pacientes, setPacientes] = useState([]);
+    const [paciente, setPaciente] = useState({});
 
     return (
         <div className="container mx-auto">
             <Header/>
             <div className="mt-12 md:flex gap-4">
-                <Formulario setPacientes={setPacientes} pacientes={pacientes}/>
-                <ListadoPaciente pacientes={pacientes}/>
+                <Formulario 
+                    setPacientes={setPacientes} 
+                    pacientes={pacientes}
+                    paciente={paciente}
+                />
+                <ListadoPaciente 
+                    pacientes={pacientes} 
+                    setPaciente={setPaciente}
+                />
             </div>
         </div>
     );
